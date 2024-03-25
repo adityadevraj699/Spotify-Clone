edaa4f383afed56eabbdb2ea00679e9166a31a0f
Aditya kumar <137451161+adityadevraj699@users.noreply.github.com>
2024-03-26 00:18:16 +0530

    Add files via upload

---
+++ Music/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import include, path
+from Music import views
+ 
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('signup', views.signup, name='signup'),
+    path('logout', views.logout, name='logout'),
+    path('music/<str:pk>', views.music, name='music'),
+    path('profile/<str:pk>', views.profile, name='profile'),
+    path('search', views.search, name='search'),
+ ]
